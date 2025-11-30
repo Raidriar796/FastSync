b4cbffe6a76d41cf784f7ed238c80c1817bcd7f7
Raidriar796 <145519075+Raidriar796@users.noreply.github.com>
2025-11-29 18:06:39 -0800

    Project setup

---
+++ FastSync/FastSync.cs
@@ -0,0 +1,22 @@
+﻿using ResoniteModLoader;
+using HarmonyLib;
+using FrooxEngine;
+
+namespace FastSync;
+
+public partial class FastSync : ResoniteMod
+{
+    public override string Name => "FastSync";
+    public override string Author => "Raidriar796";
+    public override string Version => "1.0.0";
+    public override string Link => "https://github.com/Raidriar796/FastSync";
+    private static ModConfiguration? Config;
+
+    public override void OnEngineInit()
+    {
+        Harmony harmony = new Harmony("net.raidriar796.FastSync");
+        Config = GetConfiguration();
+        Config?.Save(true);
+        harmony.PatchAll();
+    }
+}
